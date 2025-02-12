80fdc45697d2dd9010bab1b3f94d2e7a5c8c91e2
Raj Shinde <raj.shinde@netcorecloud.com>
2025-02-12 19:40:24 +0530

    initial commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>Hi</div>
+    </>
+  )
+}
+
+export default App
